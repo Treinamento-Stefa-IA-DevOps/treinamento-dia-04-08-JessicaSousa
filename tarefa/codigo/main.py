7af04e04837c88c67ee8dd1dda4e72de0745d282
jessica <jessicasousa.pc@gmail.com>
2021-08-05 19:50:54 -0300

    :sparkles: feat: adicionada verificação dos parâmetros

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,11 +1,14 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
 
 class Response(BaseModel):
     survived: bool
     status: int = 200
     message: str = "success"
+
 
 app = FastAPI(
     title="API de sobrevivência no Titanic",
@@ -13,26 +16,48 @@ app = FastAPI(
     contact={
         "name": "Jessica Sousa",
         "url": "https://github.com/Treinamento-Stefa-IA-DevOps/treinamento-dia-04-08-JessicaSousa",
-    }
+    },
 )
+
+
 @app.post("/model", response_model=Response)
-def titanic(sex: int, age: float, lifeboat: int, p_class: int):
-    """Prediz se alguém sobreviveria ao acidente do Titanic com base na:
-    
+# https://en.wikipedia.org/wiki/Lifeboats_of_the_Titanic
+def titanic(
+    sex: int = Query(..., alias="Sex", title="Sex", ge=0, le=1),
+    age: float = Query(..., alias="Age", title="Age in years", ge=0, le=120),
+    lifeboat: int = Query(
+        ..., alias="Lifeboat", title="Lifeboat", ge=1, le=20
+    ),
+    p_class: int = Query(
+        ..., alias="Pclass", title="Ticket class", ge=1, le=3
+    ),
+):
+    """Prediz se alguém sobreviveria ao acidente do Titanic com base em:
+
     - **sex**: sexo da pessoa, sendo o valor 0 para masculino e 1 para feminino
     - **age**: idade informada em modo fracionário, exemplo: 10.6
     - **lifeboat**: número do barco salva vidas utilizado
     - **p_class**: classe no navio, sendo 1 primeira classe e 3 econômica
     """
-    with open("codigo/model/Titanic.pkl", "rb") as fid: 
-        clf = pickle.load(fid)
-        pred = clf.predict([[sex, age, lifeboat, p_class]])
-        return {
-            "survived": bool(pred[0]),
-            "status": 200,
-            "message": "success",
-        }
-        
+    try:
+        with open("codigo/model/Titanic.pkl", "rb") as fid:
+            clf = pickle.load(fid)
+            pred = clf.predict([[sex, age, lifeboat, p_class]])
+            return {
+                "survived": bool(pred[0]),
+                "status": 200,
+                "message": "success",
+            }
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "detail": f"{e}",
+                "status": 500,
+                "message": "Internal Server Error.",
+            },
+        )
+
 
 @app.get("/")
 def get():
